e4197e8865051577be0a89718b29f1542ae7f1bb
Felix <felix-schindler@outlook.com>
2023-06-14 22:26:05 +0200

    db -> Counter[Model]

--- utils/db.ts
+++ utils/db.ts
@@ -2,11 +2,25 @@ const kv = await Deno.openKv();
 
 const PREFIX = ["count"];
 
-export async function getCount() {
-	const res = await kv.get<number>(PREFIX);
-	return res.value;
-}
+export default class Counter extends EventTarget {
+	public static shared = new Counter();
 
-export async function setCount(newCount: number) {
-	await kv.set(PREFIX, newCount);
+	private constructor() {
+		super();
+	}
+
+	async getCount() {
+		const res = (await kv.get<number>(PREFIX)).value;
+		if (res === null) await this.setCount(0);
+		return res ?? 0;
+	}
+
+	async setCount(newCount: number) {
+		await kv.set(PREFIX, newCount);
+		this.dispatchEvent(this.getCountEvent(newCount));
+	}
+
+	private getCountEvent(count: number) {
+		return new CustomEvent<{ count: number }>("count", { detail: { count } });
+	}
 }
