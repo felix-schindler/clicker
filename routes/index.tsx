ad1ae01e1a060b322c594d585ee07e550caae8a6
Felix <felix-schindler@outlook.com>
2023-06-15 09:04:49 +0200

    Update start page
    - Add Made by (Felix using Fresh)
    - Minor UI changes (grid, images, put counters in dropdown (except socket counter))

--- routes/index.tsx
+++ routes/index.tsx
@@ -19,36 +19,64 @@ export default function Home(props: PageProps<HomeProps>) {
 	return (
 		<>
 			<Head>
-				<title>Fresh App with Deno KV</title>
+				<title>Counter</title>
 				<meta name="color-scheme" content="dark light" />
 			</Head>
-			<body class="bg-green-200 dark:bg-green-900">
-				<div class="p-4 mx-auto max-w-screen-md flex flex-col gap-2">
-					<div class="flex flex-col items-center">
-						<img
-							src="https://dash.deno.com/assets/logo.svg"
-							class="w-32 h-32 mb-3"
-							alt="the deno logo: a dino in the rain"
-						/>
-						<p>Welcome to my <span class="font-serif">Counter</span></p>
-						<p class="text-gray-700 dark:text-gray-300">
-							Try updating this message in the
-							./routes/index.tsx file, and refresh.
-						</p>
-					</div>
-					<div>
-						<h2 class="text-xl font-bold">Counter</h2>
-						<Counter start={props.data.start} />
-					</div>
-					<div>
-						<h2 class="text-xl font-bold">Socket</h2>
-						<SocketClient />
-					</div>
+			<body class="min-h-screen max-w-screen-md mx-auto
+				grid grid-rows-hmf
+				bg-green-200 dark:bg-green-900
+			">
+				<header class="p-4 text-center">
+					<img
+						src="/logo.svg"
+						class="w-32 h-32 mx-auto mb-3"
+						alt="the deno logo: a dino in the rain"
+					/>
+					<p class="text-lg">
+						Welcome to my <span class="font-serif">Counter</span>
+					</p>
+					<p class="text-gray-700 dark:text-gray-300">
+						using Deno Deploy, Fresh, Deno KV, BroadcastChannel and WebSockets
+					</p>
+				</header>
+				<div class="p-4 flex flex-col gap-4">
 					<div>
 						<h2 class="text-xl font-bold">Socket Counter</h2>
-						<SocketCounter />
+						<SocketCounter start={props.data.start} />
 					</div>
+					<details>
+						<summary class="cursor-pointer">Ping socket server</summary>
+						<div class="mt-1">
+							<h2 class="text-xl font-bold">Socket</h2>
+							<SocketClient />
+							<p class="text-sm text-gray-700 dark:text-gray-300">
+								If the Socker Counter doesn't update, try to ping the server
+							</p>
+						</div>
+					</details>
+					<details>
+						<summary class="cursor-pointer">Non-realtime counter</summary>
+						<div class="mt-1">
+							<h2 class="text-xl font-bold">Counter</h2>
+							<Counter start={props.data.start} />
+							<p class="text-sm text-gray-700 dark:text-gray-300">
+								Go slow on this one, clicking too fast might crash the website
+								on your browser
+							</p>
+						</div>
+					</details>
 				</div>
+				<footer class="p-4">
+					<p>
+						Made by{" "}
+						<a class="hover:text-underline" href="https://schindlerfelix.de">
+							Felix
+						</a>
+					</p>
+					<a href="https://fresh.deno.dev">
+						<img src="/fresh-badge.svg" alt="Made with Fresh" />
+					</a>
+				</footer>
 			</body>
 		</>
 	);
