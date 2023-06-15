9b22c2c6ee5bde56e52d5534ff9bea364646543a
Felix <felix-schindler@outlook.com>
2023-06-15 23:21:14 +0200

    Add delete event to messages api

--- routes/api/messages.ts
+++ routes/api/messages.ts
@@ -12,6 +12,13 @@ export const handler: Handlers = {
 		const { socket, response } = Deno.upgradeWebSocket(req);
 		const channel = new BroadcastChannel("messages");
 
+		socket.onmessage = async (event) => {
+			if (typeof event.data === "string" && event.data.startsWith("delete")) {
+				const id = event.data.split(":")[1];
+				await db.deleteMessage(id);
+			}
+		};
+
 		db.addEventListener("newMessage", (event) => {
 			// @ts-ignore It's a custom event, detail does exist!
 			const msg = event.detail;
@@ -21,6 +28,22 @@ export const handler: Handlers = {
 			}
 			channel.postMessage(msg);
 		});
+
+		db.addEventListener("deleteMessage", (event) => {
+			// @ts-ignore It's a custom event, detail does exist!
+			const id = event.detail;
+
+			if (socket.readyState === WebSocket.OPEN) {
+				socket.send(`delete:${id}`);
+			}
+			channel.postMessage(`delete:${id}`);
+		});
+
+		channel.onmessage = (event) => {
+			if (socket.readyState === WebSocket.OPEN) {
+				socket.send(event.data);
+			}
+		};
 
 		return response;
 	},
