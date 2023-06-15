0aa4af42e8b554db178fe4dd28c8c541c4acf6ac
Felix <felix-schindler@outlook.com>
2023-06-15 18:13:37 +0200

    Button: Add option to allow use on server

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,20 +2,23 @@ import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export function Button(
-	props: JSX.HTMLAttributes<HTMLButtonElement> & { danger?: boolean },
+	props: JSX.HTMLAttributes<HTMLButtonElement> & {
+		serverAllowed?: boolean;
+		danger?: boolean;
+	},
 ) {
 	return (
 		<button
 			{...props}
-			disabled={!IS_BROWSER || props.disabled}
+			disabled={(!props.serverAllowed && !IS_BROWSER) || props.disabled}
 			class={`
-			px-2 py-1 rounded-md
-			border(${props.danger ? "red" : "green"}-500 2)
-			hover:bg-${props.danger ? "red" : "green"}-300
-			active:bg-${props.danger ? "red" : "green"}-400
-			dark:hover:bg-${props.danger ? "red" : "green"}-700
-			dark:active:bg-${props.danger ? "red" : "green"}-600
-	`}
+				px-2 py-1 rounded-md
+				border(${props.danger ? "red" : "green"}-500 2)
+				hover:bg-${props.danger ? "red" : "green"}-300
+				active:bg-${props.danger ? "red" : "green"}-400
+				dark:hover:bg-${props.danger ? "red" : "green"}-700
+				dark:active:bg-${props.danger ? "red" : "green"}-600
+			`}
 		/>
 	);
 }
