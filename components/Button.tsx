b029ed4f098ccc1518516c675b91ba0490cb4f06
Felix <felix-schindler@outlook.com>
2023-06-16 12:53:27 +0200

    Button: Lighter hover / active color in light mode

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,19 +3,18 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export function Button(
 	props: JSX.HTMLAttributes<HTMLButtonElement> & {
-		serverAllowed?: boolean;
 		danger?: boolean;
 	},
 ) {
 	return (
 		<button
 			{...props}
-			disabled={(!props.serverAllowed && !IS_BROWSER) || props.disabled}
-			class={`
+			disabled={!IS_BROWSER || props.disabled}
+			class={props.class + `
 				px-2 py-1 rounded-md
 				border(${props.danger ? "red" : "green"}-500 2)
-				hover:bg-${props.danger ? "red" : "green"}-300
-				active:bg-${props.danger ? "red" : "green"}-400
+				hover:bg-${props.danger ? "red" : "green"}-200
+				active:bg-${props.danger ? "red" : "green"}-300
 				dark:hover:bg-${props.danger ? "red" : "green"}-700
 				dark:active:bg-${props.danger ? "red" : "green"}-600
 			`}
